2a0954b06cfd2ca313fca133305ab4efc3763db5
Ograll <stefplotva@gmail.com>
2021-03-13 08:19:16 +0300

    Added_beautiful_vision

--- core.py
+++ core.py
@@ -2,6 +2,8 @@ import sys
 import subprocess
 import pyqtgraph.opengl as gl
 from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QAction, qApp, QLabel, QPushButton, QHBoxLayout, QVBoxLayout
+from PyQt5.QtGui import QPalette, QColor, QFont
+from PyQt5.QtCore import Qt
 
 # QTabWidget
 
@@ -10,6 +12,10 @@ class MainPartOfOurApp(QMainWindow):
     def __init__(self, parent=None):
         super(MainPartOfOurApp, self).__init__(parent)
         menubar = self.menuBar()
+        menubar.setStyleSheet("""
+                    background: rgba(0, 23, 107, 0.42);
+                    color: #A5A9B8;
+                """)
         file_menu = menubar.addMenu("Файл")
         close_action = QAction("Выход", self)
         file_menu.addAction(close_action)
@@ -22,9 +28,13 @@ class MainPartOfOurApp(QMainWindow):
         visual_menu = QAction("123", self)
         test_visual_menu.addAction(visual_menu)
         visual_menu.triggered.connect(self.open_test)
+        palette = QPalette()
+        palette.setColor(QPalette.Window, QColor("#000000"))
         self.main_part_of_our_app = OtherPartsOfOurApp(parent=self)
         self.setCentralWidget(self.main_part_of_our_app)
         self.showFullScreen()
+        self.setPalette(palette)
+        self.setFont(QFont("Times", 10, QFont.Bold))
 
     def open_test(self):
         core_of_auth_form = "E:/Programming/My_course_working/other/pyqtgraphhelper.py"
@@ -64,8 +74,36 @@ class RegistrationWindow(QWidget):
 class ModalWindow(QWidget):
     def __init__(self):
         super(ModalWindow, self).__init__()
-        agitation_label = QLabel("Уважаемый пользователь! Рекомендуем авторизоваться или зарегистрироваться, "
+        stylesheet = """
+        .QLabel
+        {
+            font-size: 14pt;
+            color: #A5A9B8;
+        }
+        
+        .QPushButton
+        {
+            font-size: 12pt;
+            background: rgba(0, 41, 187, 0.73);
+            color: #A5A9B8;
+        }
+        
+        .QPushButton:hover
+        {
+            background: rgba(0, 23, 107, 0.42);
+            border: 1px solid #0029BB;
+        }
+        
+        .QPushButton:focus
+        {
+            background: rgba(0, 23, 107, 0.42);
+            border: 1px solid #0029BB;
+        }
+        """
+        agitation_label = QLabel("Уважаемый пользователь! Рекомендуем авторизоваться или зарегистрироваться,\n"
                                  "чтобы использовать все возможности приложения...", self)
+        agitation_label.adjustSize()
+        agitation_label.setAlignment(Qt.AlignCenter)
         auth_button = QPushButton("Авторизоваться", self)
         auth_button.clicked.connect(self.open_auth_form)
         reg_button = QPushButton("Зарегистрироваться", self)
@@ -81,7 +119,13 @@ class ModalWindow(QWidget):
         vbox_1.addWidget(quit_button)
         hbox_2 = QHBoxLayout()
         hbox_2.addLayout(vbox_1)
+        palette = QPalette()
+        palette.setColor(QPalette.Window, QColor("#000000"))
         self.setLayout(hbox_2)
+        self.setPalette(palette)
+        self.setStyleSheet(stylesheet)
+        self.setFont(QFont("Times", 10))
+        self.setWindowFlags(Qt.FramelessWindowHint)
 
     @staticmethod
     def close_modal_window():
@@ -101,10 +145,20 @@ class ModalWindow(QWidget):
         print(id_code_of_user)
 
 
+def move_to_center(win):
+    screen_geometry = QApplication.desktop().availableGeometry()
+    screen_size = (screen_geometry.width(), screen_geometry.height())
+    win_size = (win.frameSize().width(), win.frameSize().height())
+    x = screen_size[0] / 2 - win_size[0] / 2
+    y = screen_size[1] / 2 - win_size[1] / 2
+    win.move(int(x), int(y))
+
+
 if __name__ == "__main__":
     application = QApplication(sys.argv)
     program = MainPartOfOurApp()
     program.show()
     modal_window = ModalWindow()
     modal_window.show()
+    move_to_center(modal_window)
     sys.exit(application.exec_())
